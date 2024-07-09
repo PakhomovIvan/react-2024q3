c7995785cda231f1d468d5d6cf02574d1461091f
Ivan Pakhomov <ip1133@ya.ru>
2024-07-09 10:40:49 +0300

    feat: configure prettier & eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,37 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+  },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "prettier",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:import/errors",
+    "plugin:import/typescript",
+    "plugin:import/warnings",
+    "plugin:react/jsx-runtime"
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', "react-compiler"],
   rules: {
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-explicit-any": "error",
+    "react-compiler/react-compiler": "error",
+    'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': [
+      'warn',
+      {
+        EndOfLine: 'auto',
+      },
+    ],
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    "import/no-unresolved": "off",
   },
-}
+};
